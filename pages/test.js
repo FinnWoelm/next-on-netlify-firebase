6982b6323359febb7de0100e3b3a8e9f01827103
Finn Woelm <public@finnwoelm.com>
2020-10-31 21:20:14 +0100

    Add page using getServerSideProps and firebase
    Test if firebase works correctly.

--- pages/test.js
+++ pages/test.js
@@ -1,13 +1,43 @@
-const TestPage = () => <p>Hi</p>
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const TestPage = (props) => (
+  <>
+    <h1>getServerSideProps test:</h1>
+    <pre>props: {JSON.stringify(props, null, 2)}</pre>
+  </>
+);
 
 export async function getServerSideProps() {
-  // Do things inside Netlify-Function
+  // Configuration for firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyBre20U7moSMgS5pCKSJsde2AIcssegRys",
+    authDomain: "next-on-netlify-firebase.firebaseapp.com",
+    databaseURL: "https://next-on-netlify-firebase.firebaseio.com",
+    projectId: "next-on-netlify-firebase",
+    storageBucket: "next-on-netlify-firebase.appspot.com",
+    messagingSenderId: "150946230344",
+    appId: "1:150946230344:web:c5d3e4fa7052b1891ce87d",
+  };
 
+  // Initialize Firebase
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  // Fetch all users
+  const dbRef = firebase.database().ref();
+  const usersRef = dbRef.child("users");
+  const usersQuery = await usersRef.once("value");
+  const users = usersQuery.val();
+
+  // Send props to page
   return {
     props: {
-
-    }
-  }
+      users,
+    },
+  };
 }
 
-export default TestPage
+export default TestPage;
